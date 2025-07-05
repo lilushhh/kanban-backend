c3620943bdf9a4b5eb214d55e65bc478f615e9ff
Lihie <lilyelimelech@gmail.com>
2025-07-05 18:39:39 +0300

    feat: added conversion functions from orm to pydantic and vice versa

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,6 +1,10 @@
 from enum import Enum
-from typing import Optional
 from uuid import UUID
+from models.orm.orm_types.project_orm import ProjectORM
+from models.orm.orm_types.task_orm import TaskORM
+from models.domain.types.project import Project
+from models.domain.types.task import Task
+
 
 def to_serializable(obj):
     if isinstance(obj, UUID):
@@ -8,3 +12,37 @@ def to_serializable(obj):
     if isinstance(obj, Enum):
         return obj.value
     return str(obj)
+
+def project_to_pydantic(project_orm: ProjectORM) -> Project:
+    return Project(
+        id = project_orm.id,
+        type = project_orm.type,
+        name = project_orm.name,
+        users = project_orm.users
+    )
+
+def project_to_orm(project_pydantic: Project) -> ProjectORM:
+    return ProjectORM(
+        id = project_pydantic.id,
+        type = project_pydantic.type,
+        name = project_pydantic.name,
+        users = project_pydantic.users
+    )
+
+def task_to_pydantic(task_orm: TaskORM) -> Task:
+    return Task(
+        id = task_orm.id,
+        type = task_orm.type,
+        owners = task_orm.owners,
+        status = task_orm.status,
+        project_id = task_orm.project_id
+    )
+
+def task_to_orm(task_pydantic: Task) -> TaskORM:
+    return TaskORM(
+        id = task_pydantic.id,
+        type = task_pydantic.type,
+        owners = task_pydantic.owners,
+        status = task_pydantic.status,
+        project_id = task_pydantic.project_id
+    )
