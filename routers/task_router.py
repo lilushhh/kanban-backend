684e40530a3efa1a386780869ac59e6c79c965cc
lilushhh <lilyelimelech@gmail.com>
2025-07-07 11:19:09 +0300

    feat: added DI to all routers

--- routers/task_router.py
+++ routers/task_router.py
@@ -10,24 +10,23 @@ from models.requestes.task_requests import (
 )
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
-task_repo = TaskRepositoryDB()
 
 def get_task_repo(db: AsyncSession = Depends(get_db)) -> TaskRepositoryDB:
     return TaskRepositoryDB(db)
 
 @router.get("/")
-def read_tasks():
+def read_tasks(task_repo: TaskRepositoryDB = Depends(get_task_repo)):
     return task_repo.get_all()
 
 @router.get("/{task_id}")
-def read_task_by_id(task_id: UUID):
+def read_task_by_id(task_id: UUID, task_repo: TaskRepositoryDB = Depends(get_task_repo)):
     task = task_repo.get_by_id(task_id)
     if task:
         return task
     raise HTTPException(status_code=404, detail="task not found")
 
 @router.post("/")
-def add_task(task: CreateTaskRequest):
+def add_task(task: CreateTaskRequest, task_repo: TaskRepositoryDB = Depends(get_task_repo)):
     task_new = Task(
         text=task.task_title,
         owners=task.owners_list,
@@ -40,7 +39,7 @@ def add_task(task: CreateTaskRequest):
     raise HTTPException(status_code=400, detail="cannot create task")
 
 @router.put("/{task_id}")
-def update_task(task_id: UUID, update: UpdateTaskRequest):
+def update_task(task_id: UUID, update: UpdateTaskRequest, task_repo: TaskRepositoryDB = Depends(get_task_repo)):
     task_to_update = Task(
         id=task_id,
         text=update.task_title,
@@ -54,7 +53,7 @@ def update_task(task_id: UUID, update: UpdateTaskRequest):
     raise HTTPException(status_code=404, detail="task not found")
 
 @router.delete("/{task_id}")
-def delete_task(task_id: UUID):
+def delete_task(task_id: UUID, task_repo: TaskRepositoryDB = Depends(get_task_repo)):
     task_to_delete = task_repo.delete_item(task_id)
     if not task_to_delete:
         raise HTTPException(status_code=404, detail="cannot find task to delete")
