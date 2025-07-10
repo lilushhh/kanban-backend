29e451354eef5c2b8428be8f478601a216ee1a57
lilushhh <lilyelimelech@gmail.com>
2025-07-10 09:41:20 +0300

    feat: added auxiliary function to create project with invalid cases

--- tests/test_task_router.py
+++ tests/test_task_router.py
@@ -7,8 +7,9 @@ from models.enums.status_enum import StatusEnum
 client = TestClient(app)
 
 def create_project():
+    unique_name = f"Task Test Project {uuid4()}"
     payload = {
-        "name_project": "Task Test Project",
+        "name_project": unique_name,
         "users_list": ["user1", "user2"]
     }
     response = client.post("/projects/", json=payload)
@@ -32,3 +33,36 @@ def test_create_task_success():
     assert data["status"] == "todo"
     assert data["project_id"] == project_id
     assert "id" in data
+
+@pytest.mark.integration
+def test_create_task_invalis_cases():
+    fake_project_id = str(uuid4())
+    payload_fake_id = {
+        "project_id": fake_project_id,
+        "task_title": "Task With Fake Project",
+        "owners_list": ["user1"],
+        "status_task": "todo"
+    }
+    res_fake_id = client.post("/tasks/", json=payload_fake_id)
+    assert res_fake_id.status_code == 400
+    assert res_fake_id.json()["detail"] == "cannot create task"
+
+    real_project_id = create_project()
+    payload_invalid_users = {
+        "project_id": real_project_id,
+        "task_title": "Task With Invalid Owners",
+        "owners_list": ["not_in_project"],
+        "status_task": "todo"
+    }
+    res_invalid_users = client.post("/tasks/", json=payload_invalid_users)
+    assert res_invalid_users.status_code == 400
+    assert res_invalid_users.json()["detail"] == "cannot create task"
+
+    payload_missing_fields = {
+        "project_id": real_project_id,
+        "owners_list": ["user1"],
+        "status_task": "todo"
+    }
+    res_missing_fields = client.post("/tasks/", json=payload_missing_fields)
+    assert res_missing_fields.status_code == 422
+
